859ffe2b25ef1964b67c2759bbfe829b0ce0d772
RomanVoronezh <vocadusomar@gmail.com>
2022-08-03 11:27:07 +0300

    gg solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {        
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return  this.average = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
